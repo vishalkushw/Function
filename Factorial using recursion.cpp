5992fdce470af9363f98620e0f04749f6ce7215b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-09 20:32:57 +0530

    Factorial using recursion

---
+++ Factorial using recursion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n-1);
+}
+int main(){
+	int num;
+	cout<<"Enter number : ";
+	cin>>num;
+	cout<<factorial(num);
+}
