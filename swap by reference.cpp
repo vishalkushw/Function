20729ba1a9caaf2ae2c131bece4dfa69f6b5111e
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-25 11:45:54 +0530

    swap by reference

---
+++ swap by reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a ,int &b){
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"After swapping "<<"\n";
+	cout<<"Value of a = "<<a<<"\n";
+	cout<<"Value of b = "<<b<<"\n";
+}
+int main(){
+	int x,y;
+	cout<<"Enter two number : ";
+	cin>>x>>y;
+	cout<<"Before swapping "<<"\n";
+	cout<<"Value of x = "<<x<<"\n";
+	cout<<"Value of x = "<<y<<"\n";
+	swap(x,y);
+}
