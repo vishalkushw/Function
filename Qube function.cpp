7c1e30ea0689b8538791ea8c49f024f8d89c91ce
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-06 18:13:02 +0530

    Qube function

---
+++ Qube function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void sqr(int n){
+	
+	 cout<<"Number qube is "<<n*n*n;
+	
+}
+
+int main(){
+	int n;
+	cout<<"Enter number : ";
+	cin>>n;
+   
+   sqr(n);
+	
+}
