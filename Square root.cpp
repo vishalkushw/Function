5b5ed8e348445b986516e61e895291f9e816bbc6
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-06 18:13:31 +0530

    Square root

---
+++ Square root.cpp
@@ -0,0 +1,43 @@
+/*Reusablity
+  Memory saving
+  Time Saving
+  
+             Functions
+    Predefined          userdefined
+    
+    #include <iostream>    c++ class file
+    #include <math.h>      c header file
+    
+*/    
+
+//#include <iostream>
+//using namespace std;
+//#include <math.h>
+//int main(){
+//	int num;
+//	cout<<"Enter any number: ";
+//	cin>>num;
+//	
+//	cout<<"square root : "<<sqrt(num)<<endl;
+//	cout<<"power : "<<pow(3,2);
+//	
+//}
+
+
+////#include <math.h>
+//int main(){
+//	char c[10]="welcomesz";
+////	c="wellcomez";     //error constant we cant reinisilize
+//	cout<<c;
+//}
+
+#include <iostream>
+using namespace std;
+#include <string.h>
+#include <math.h>
+int main(){
+	char c[10];
+	cout<<"Enter name: ";
+	cin>>c;
+	cout<<"name="<<c;	
+}
